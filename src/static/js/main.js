6e48a3e5d61d1ccf11651ad0caf9ee78012ed457
Fedorov Nikita <fedorovni1996@yandex.ru>
2020-07-15 11:58:48 +0500

    Лого интерсвязи и bg для мобилки

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,15 +1,28 @@
 let sandwich = document.querySelector('.header__sandwich');
 let menu = document.querySelector('.nav__list');
-let navItem = document.querySelectorAll('.nav__item');
+let navItems = document.querySelectorAll('.nav__item');
+let anchors = document.querySelectorAll('a[href*="#"]')
+
 
 sandwich.addEventListener('click', () => {
   sandwich.classList.toggle('sandwich--active');
   menu.classList.toggle('nav__list--active');
 })
 
-for (let i = 0; i < navItem.length; i++) {
-  navItem[i].addEventListener('click', () => {
+for (let i = 0; i < navItems.length; i++) {
+  navItems[i].addEventListener('click', () => {
     sandwich.classList.toggle('sandwich--active');
     menu.classList.toggle('nav__list--active');
+  })
+}
+
+for (let i = 0; i < anchors.length; i++) {
+  anchors[i].addEventListener('click', (e) => {
+    e.preventDefault();
+    let blockId = anchors[i].getAttribute('href');
+    document.querySelector('' + blockId).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
   })
 }
